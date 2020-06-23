7fec240d3ca9b84056cc67bf4f18574b3ee7799b
neuxxm <neuxxm@gmail.com>
2020-06-23 12:54:44 +0800

    067 math

--- problems/067/test.py
+++ problems/067/test.py
@@ -1,4 +1,4 @@
-#18:19-18:21
+#12:48AC
 class Solution(object):
     def addBinary(self, a, b):
         """
@@ -6,15 +6,15 @@ class Solution(object):
         :type b: str
         :rtype: str
         """
-        tag = 0
         n = len(a)
         m = len(b)
-        i = n - 1
-        j = m - 1
+        i = n-1
+        j = m-1
         r = ''
-        while i >= 0 and j >= 0:
+        tag = 0
+        while i>=0 and j>=0:
             t = int(a[i]) + int(b[j]) + tag
-            if t > 1:
+            if t >= 2:
                 t -= 2
                 tag = 1
             else:
@@ -22,18 +22,18 @@ class Solution(object):
             r += '%d'%t
             i -= 1
             j -= 1
-        while i >= 0:
+        while i>=0:
             t = int(a[i]) + tag
-            if t > 1:
+            if t >= 2:
                 t -= 2
                 tag = 1
             else:
                 tag = 0
             r += '%d'%t
             i -= 1
-        while j >= 0:
+        while j>=0:
             t = int(b[j]) + tag
-            if t > 1:
+            if t >= 2:
                 t -= 2
                 tag = 1
             else:
