bd81ee73d5f7036b741b198e89ad84414b22db2a
neuxxm <neuxxm@gmail.com>
2020-06-25 16:42:27 +0800

    076 sliding-window

--- problems/076/test.py
+++ problems/076/test.py
@@ -1,50 +1,46 @@
-#15:51-16:20
-class Solution(object):
-    def minWindow(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: str
-        """
-        need = {}
-        cnt = {}
-        valid = 0
-        for c in t:
-            if c in need:
-                need[c] += 1
-            else:
-                need[c] = 1
-                cnt[c] = 0
-        need_len = len(need)
-        n = len(s)
-        l = 0
-        r = 0
-        z = n+1
-        zs = ''
-        while True:
-            while r < n:
-                c = s[r]
-                r += 1
-                if c in need:
-                    cnt[c] += 1
-                    if cnt[c] == need[c]:
-                        valid += 1
-                        if valid == need_len:
-                            break
-            if valid != need_len:
-                break
-            while l < n:
-                c = s[l]
-                l += 1
-                if c in need:
-                    if cnt[c] == need[c]:
-                        cnt[c] -= 1
-                        valid -= 1
-                        break
-                    else:
-                        cnt[c] -= 1
-            t = r-l+1
-            if t < z:
-                z = t
-                zs = s[l-1:r]
-        return zs
+#6.25 15:49-16:02
+class Solution(object):
+    def minWindow(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: str
+        """
+        need = {}
+        cnt = {}
+        for c in t:
+            if c in need:
+                need[c] += 1
+            else:
+                need[c] = 1
+            cnt[c] = 0
+        m = len(cnt)
+        n = len(s)
+        l = 0
+        r = 0
+        z = 0
+        ans = ''
+        ans_len = n + 1
+        while r < n:
+            while r < n:
+                c = s[r]
+                r += 1
+                if c in need:
+                    cnt[c] += 1
+                    if cnt[c] == need[c]:
+                        z += 1
+                        if z == m:
+                            break
+            while z == m:
+                c = s[l]
+                t = r - l
+                if t < ans_len:
+                    ans = s[l:r]
+                    ans_len = t
+                l += 1
+                if c in need:
+                    cnt[c] -= 1
+                    if cnt[c] < need[c]:
+                        z -= 1
+                        break
+        return ans if ans_len != n+1 else ''
