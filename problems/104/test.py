a9b199610bd0202c2c6284e0e3052268dda6dad0
neuxxm <neuxxm@gmail.com>
2020-07-26 19:28:23 +0800

    104 binary-tree, dfs

--- problems/104/test.py
+++ problems/104/test.py
@@ -1,23 +1,18 @@
-# Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-#20:17-20:20
-def h(x):
-    if x == None:
-        return 0
-    r = 1
-    if x.left:
-        r = max(r, h(x.left) + 1)
-    if x.right:
-        r = max(r, h(x.right) + 1)
-    return r
-class Solution(object):
-    def maxDepth(self, root):
-        """
-        :type root: TreeNode
-        :rtype: int
-        """
-        return h(root)
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+#19:22-19:23
+def f(x):
+    if x == None:
+        return 0
+    return max(f(x.left), f(x.right)) + 1
+class Solution(object):
+    def maxDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        return f(root)
