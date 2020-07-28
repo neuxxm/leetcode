0829c449b0b6e9bccf0b9ac17b30dea954e9015d
neuxxm <neuxxm@gmail.com>
2020-07-28 16:25:05 +0800

    543 binary-tree, diameter-sum

--- problems/543/test.py
+++ problems/543/test.py
@@ -4,35 +4,22 @@
 #         self.val = x
 #         self.left = None
 #         self.right = None
-#6.26 20:36-20:44
+#16:18-16:21
 def f(x, z):
-    left = 0
-    right = 0
-    if x.left:
-        left = f(x.left, z)
-    if x.right:
-        right = f(x.right, z)
-    h = 0
-    if x.left or x.right:
-        h = max(left, right) + 1
-        if x.left and x.right:
-            t = left + right + 2
-        elif x.left:
-            t = left + 1
-        else:
-            t = right + 1
-        if t > z[0]:
-            z[0] = t
-    #print x.val, h, z[0]
-    return h
+    if x == None:
+        return 0
+    l = f(x.left, z)
+    r = f(x.right, z)
+    t = l + r
+    if t > z[0]:
+        z[0] = t
+    return max(l, r) + 1
 class Solution(object):
     def diameterOfBinaryTree(self, root):
         """
         :type root: TreeNode
         :rtype: int
         """
-        if root == None:
-            return 0
         z = [0]
         f(root, z)
         return z[0]
